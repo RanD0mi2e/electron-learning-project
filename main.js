f774bf3e411f2919180750d3dc04ea2887f2d7cb
pangzijian <871119445@qq.com>
2023-08-29 11:43:26 +0800

    demo2

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
+// 自动更新
+require('update-electron-app')()
 
 const createWindow = () => {
   const win = new BrowserWindow({
